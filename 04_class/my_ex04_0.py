c1d655485d6bac8cd640fb2153818d4cc83034d8
k-itashiki <kunimitsu.itashiki.cw@hitachi.com>
2019-05-10 15:35:45 +0900

    Modify my_ex04_0.py file

--- 04_class/my_ex04_0.py
+++ 04_class/my_ex04_0.py
@@ -4,8 +4,8 @@ import unittest
 
 class Bot():
 
-    def __init__(self, owner_name):
-        self.owner_name = "A"
+    def __init__(self, name):
+        self.owner_name = name
     
     def reply(self, call):
         if self.owner_name in call:
@@ -18,7 +18,7 @@ class TestBot(unittest.TestCase):
 
     def test_bot_reply(self):
         bot = Bot("Angy")
-        self.assertEqual("Hello", bot.reply("Hi, I'm Bill.'"))
+        self.assertEqual("Hello", bot.reply("Hi, I'm Bill."))
         self.assertEqual("Hello my Boss!", bot.reply("Hi, I'm Angy."))
 
 
